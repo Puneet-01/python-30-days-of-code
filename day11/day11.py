a15e8c077e0751ab48cb68058422910fe87d466d
puneet <puneet.tiwari.2023@gmail.com>
2021-01-13 08:42:54 +0530

    Day11:Inheritance

--- day11/day11.py
+++ day11/day11.py
@@ -1,17 +1,41 @@
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
 
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber,marks):
+        super().__init__(firstName, lastName, idNumber)
+        self.marks=marks
+    def calculate(self):
+        x=len(self.marks)
+        sum=0
+        for i in range(x):
+            sum=sum+self.marks[i]
+        avg=sum/x
+        if avg>=90 :
+            return 'O'
+        elif avg>=80 :
+            return 'E'
+        elif avg>=70 :
+            return 'A'
+        elif avg>=55 :
+            return 'P'
+        elif avg>=40:
+            return 'D'
+        else :
+            return 'T'
 
-
-if __name__ == '__main__':
-    arr = []
-
-    for _ in range(6):
-        arr.append(list(map(int, input().rstrip().split())))
-sum=0
-max=-100
-for i in range(4):
-    for j in range(4):
-        sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]
-        sum=sum+arr[i+2][j+2]
-        if sum>max :
-            max=sum
-print(max) 
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
